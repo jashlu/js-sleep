83da464bbaa1a3ec556544b27e4d1ca8cd597c38
Josh <joshua_lu1@brown.edu>
2024-12-27 23:22:27 -0500

    init important files

---
+++ README.md
@@ -0,0 +1,1 @@
+A demo package for sleep functionality
---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Prints "Hello!" to the console a given number of times
+ * @param ms intended number of milliseconds to sleep for
+ */
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  module.exports = sleep
