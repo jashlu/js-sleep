db3743cbd0fa51059d9082c9f5b3763d6d97ea9d
Josh <joshua_lu1@brown.edu>
2024-12-27 20:53:56 -0500

    add module exports

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
  * Prints "Hello!" to the console a given number of times
  * @param ms intended number of milliseconds to sleep for
  */
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-export function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+module.exports = sleep
