ddf9e4e5b4d1b217de651638449c8827724b7650
Josh <joshua_lu1@brown.edu>
2024-12-27 23:36:09 -0500

    add error checking

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 /**
- * Prints "Hello!" to the console a given number of times
+ * Adds delay in execution of a program
  * @param ms intended number of milliseconds to sleep for
  */
 function sleep(ms) {
+    if (typeof ms !== "number" || ms < 0){
+        throw new Error("Invalid time parameter")
+    }
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  
-  module.exports = sleep
+
+module.exports = sleep
